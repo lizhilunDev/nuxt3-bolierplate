433de1fedd0f3f46b3a20da1a3160dde651f2549
jiyoonlee <im.lizhilun@gmail.com>
2024-04-21 02:58:27 +0900

    layouts 추가

---
+++ layouts/default.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+const pageContainerStyle = computed(() => ({
+  maxWidth: '1080px',
+  margin: '0 auto',
+}));
+</script>
+
+<template>
+  <q-layout view="hHh lpR fFf" class="bg-grey-2">
+    <q-header elevated class="bg-dark text-white">
+      <q-toolbar>
+        <q-toolbar-title> Vue & Nuxt Mastery Class </q-toolbar-title>
+        <NuxtLink v-slot="{ navigate }" custom to="/">
+          <q-btn stretch flat label="Home" no-caps @click="navigate" />
+        </NuxtLink>
+        <q-separator dark vertical />
+        <NuxtLink v-slot="{ navigate }" to="/about" custom>
+          <q-btn stretch flat label="About" no-caps @click="navigate" />
+        </NuxtLink>
+      </q-toolbar>
+    </q-header>
+    <q-page-container :style="pageContainerStyle">
+      <slot></slot>
+    </q-page-container>
+  </q-layout>
+</template>
